49103c6dfd778466e8073ed9e81110231b6c3e4b
Mugilvannan P <mugilvannanpichai02@gmail.com>
2025-04-02 19:36:54 +0530

    add new skills and experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,7 +100,7 @@ function showSkills(skills) {
         skillHTML += `
         <div class="bar">
               <div class="info">
-                <img src=${skill.icon} alt="skill" />
+                <img src=${skill.icon} width="48" alt="skill" />
                 <span>${skill.name}</span>
               </div>
             </div>`
